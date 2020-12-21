7208ebc120d2ee5e84fa988807de45e20ed164f9
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2020-12-22 01:05:30 +0800

    wrote svg arrow to hint user to scroll down

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,60 @@ import { DarkThemeWrapper, DarkToggle } from "../components/DarkTheme";
 import { genBg } from "../components/isogrid";
 import { useEffect } from "react";
 
-export default function Index(){
+function DownArrow(){
+	//TODO took me a while to write, will generalise next time
+	return (
+		<svg viewBox="0 0 100 155" xmlns="http://www.w3.org/2000/svg" version="1.1" height="100%" width="100%">
+			<g>
+				<polyline stroke="currentColor" stroke-width="8" stroke-linecap="butt"  fill="none" 
+					points="0,0 50,50 100,0"
+				></polyline>
+				<polyline stroke="currentColor" stroke-width="8" stroke-linecap="butt"  fill="none" 
+					points="0,50 50,100 100,50"
+				></polyline>
+				<animateTransform
+					attributeName="transform"
+					type="translate"
+					values="0 5 ; 0 50"
+					calcMode="spline"
+					keyTimes="0 ; 1"
+					keySplines="0 0 0.5 1"
+					dur="1s"
+					repeatCount="indefinite"
+				/>
+				<animate
+					attributeName="opacity"
+					values="0; 1 ; 0"
+					calcMode="spline"
+					keyTimes="0 ; 0.3 ; 1"
+					keySplines="0.5 0 0.5 1 ; 0.5 0 0.5 1"
+					dur="1s"
+					repeatCount="indefinite"
+				/>
+			</g>
+		</svg>
+	);
+}
+
+function Banner(){
+	useEffect(() => {
+		let bg = genBg({rows:6,cols:6});
+		bg.setAttribute("height","100%");
+		bg.setAttribute("width","100%");
+		document.querySelector('#banner-img-wrapper')?.appendChild(bg);
+		return () => bg.remove();
+	});
+
+	return (
+	<header id="banner" className={`h-screen max-w-screen text-center`}>
+		<div id="banner-img-wrapper" className={`absolute flex h-screen w-full overflow-hidden justify-center -z-10 bg-yellow-200`}></div> {/* h-screen rather than h-full some weird margin issue */}
+		<h1 className={`absolute text-5xl bg-white bg-opacity-70 rounded px-1 bottom-64 mx-auto right-0 left-0 md:text-7xl md:bottom-32 md:px-auto md:ml-5 md:m-0 md:right-auto`}>TODO: non-cringy tagline</h1>
+		<div className={`absolute mx-auto bottom-0 right-0 left-0 h-20 text-white`}><DownArrow/></div>
+	</header>
+	);
+}
+
+function Main(){
 	let cards:CardData[] = [
 		{
 			link:"https://github.com/Interpause/interpause-dev",
@@ -39,15 +92,38 @@ export default function Index(){
 		}
 	];
 
-	useEffect(() => {
-		let bg = genBg({rows:6,cols:6});
-		bg.setAttribute("height","100%");
-		bg.setAttribute("width","100%");
-		document.querySelector('#banner-img-wrapper')?.appendChild(bg);
-		return () => bg.remove();
-	})
+	return (
+		<DarkThemeWrapper darkDefault={true}>
+			<section className={`text-center min-h-screen transition-colors bg-white text-black dark:bg-black dark:text-white`}>
+				<h1 className={`text-5xl py-4 font-bold`} style={{fontFamily:"Comic Sans MS, Comic Sans, cursive"}}>UNDER CONSTRUCTION</h1>
+					<DarkToggle/>
+					<div className={`flex justify-center font-mono`}>
+						<CardFlex cards={cards}/>
+					</div>
+				<Counter/>
+			</section>
+		</DarkThemeWrapper>
+	);
+}
 
-	//TODO componentize banner and footer. Add animated arrow thingy telling you to scroll down. 
+function Footer(){
+	return (
+		<DarkThemeWrapper>
+			<footer className={`absolute w-full border-t-2 transition-colors border-gray-200 bg-gray-50 text-black dark:border-gray-900 dark:bg-gray-900 dark:text-white`}>
+				<span className={`absolute right-1 top-1`}><DarkToggle/></span>
+				<h4 className={`text-lg text-center pt-8 sm:pt-1`}>TODO: footer{' '}
+					<a
+						className={`no-underline hover:underline text-center text-blue-400`}
+						href="#"
+					>link that goes nowhere... so far</a>
+				</h4>
+				<h6 className={`block text-sm text-center text-gray-500 py-1`}>© {new Date().getFullYear()} Interpause</h6>
+			</footer>
+		</DarkThemeWrapper>
+	);
+}
+
+export default function Index(){
 	return (
 		<div>
 			<Head>
@@ -56,38 +132,16 @@ export default function Index(){
 				<meta name="topic" content="portfolio"/>
 				<meta name="keywords" content="interpause, developer, maker"/>
 			</Head>
-			<header id="banner" className={`h-screen max-w-screen text-center`}>
-				<div id="banner-img-wrapper" className={`absolute flex h-screen w-full overflow-hidden justify-center -z-10 bg-yellow-200`}></div> {/* h-screen rather than h-full some weird margin issue */}
-				<h1 className={`absolute text-5xl bg-white bg-opacity-70 rounded px-1 bottom-64 mx-auto right-0 left-0 md:text-7xl md:bottom-32 md:px-auto md:ml-5 md:m-0 md:right-auto`}>TODO: non-cringy tagline</h1>
-			</header>
-			<DarkThemeWrapper darkDefault={true}>
-			<div className={`text-center transition-colors bg-white text-black dark:bg-black dark:text-white`}>
-				<h1 className={`text-5xl py-4 font-bold`} style={{fontFamily:"Comic Sans MS, Comic Sans, cursive"}}>UNDER CONSTRUCTION</h1>
-					<DarkToggle/>
-					<div className={`flex justify-center font-mono`}>
-						<CardFlex cards={cards}/>
-					</div>
-				<Counter/>
-			</div>
-			</DarkThemeWrapper>
-			<DarkThemeWrapper>
-			<footer className={`border-t-2 text-center transition-colors border-gray-200 bg-gray-50 text-black dark:border-gray-900 dark:bg-gray-900 dark:text-white`}>
-				<h4 className={`text-lg`}>TODO: footer{' '}
-					<a
-						className={`no-underline hover:underline text-blue-400`}
-						href="#"
-					>link that goes nowhere... so far</a>
-				</h4>
-				<span><DarkToggle/></span>
-			</footer>
-			</DarkThemeWrapper>
+			<Banner/>
+			<Main/>
+			<Footer/>
 		</div>
 	)
 }
 
 /*
- * TODO: can a CMS like Wordpress pipe .mdx files to the blog subdomain? MDX sounds damn cool https://mdxjs.com/ TBH: blog subdomain should be separate repo
- * TODO: make toggle switch and counter even though they are useless.
+ * TODO: can a CMS like Wordpress (what cms) pipe .mdx files to the blog subdomain? MDX sounds damn cool https://mdxjs.com/ TBH: blog subdomain should be separate repo
+ * TODO: make counter even though they are useless.
  * TODO: Visit other .dev sites to get inspiration (much later)
  * TODO: make Under construction rainbow and comic sans and marquee
  * TSDocs: https://tsdoc.org/pages/tags/alpha/

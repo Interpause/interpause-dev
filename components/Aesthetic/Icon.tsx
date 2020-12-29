75d266e00b80ab4f2e85d3d25d22cdca1aedca21
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2020-12-29 19:40:21 +0800

    finally got icon typings correct

--- components/Aesthetic/Icon.tsx
+++ components/Aesthetic/Icon.tsx
@@ -3,8 +3,9 @@
  * @author John-Henry Lim <hyphen@interpause.dev>
  */
 import { css } from 'twin.macro';
-import Image from "next/image";
+import Image, { ImageProps } from "next/image";
 import { OrientableSVG, Orientation } from "./index";
+
 export enum ICON {
 	/** https://icomoon.io/ */
 	facebook='M608 192h160v-192h-160c-123.514 0-224 100.486-224 224v96h-128v192h128v512h192v-512h160l32-192h-192v-96c0-17.346 14.654-32 32-32z',
@@ -12,13 +13,13 @@ export enum ICON {
 	menu='M64 288A1 1 0 0164 224H960A1 1 0 01960 288H64ZM64 800A1 1 0 0164 736H960A1 1 0 01960 800H64ZM64 544A1 1 0 0164 480H960A1 1 0 01960 544H64Z'
 }
 
-type IconProps = ({orientation?:Orientation|number,className?:string,src:string,priority?:boolean,type?:never})|({type:ICON} & OrientableSVG);
-/** In className, specify width, height and textColor. Either specify type which is enum ICON or src which is path. */
+type IconProps = (({orientation?:Orientation|number,className?:string,type?:never} & Omit<ImageProps,"height"|"width"|"layout">)|({type:ICON} & OrientableSVG));
+/** Must restyle with height and width. Either specify type which is enum ICON or src which is path. */
 export function Icon({orientation,className,...props}:IconProps){
 	return <span tw="relative m-0.5 flex-none" css={css`transform:rotate(${orientation??0}deg)`} className={className}>
 		{
 			("src" in props)?
-				(<Image src={props.src} layout="fill" priority={props.priority}/>)
+				(<Image layout="fill" {...props}/>)
 			:
 				(<svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" version="1.1" height="100%" width="100%" {...props}>
 					<path strokeLinecap="round" strokeLinejoin="round" d={props.type} fill="currentColor"></path>

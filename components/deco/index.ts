d320981260d2647f9bd6edc2f48e9ae42cc1cdf3
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2021-01-02 11:42:17 +0800

    move deco back

---
+++ components/deco/index.ts
@@ -0,0 +1,19 @@
+import { SVGProps } from "react";
+
+/** All SVG components are designed oriented upwards */
+export enum Orientation {
+	up=0,
+	right=90,
+	down=180,
+	left=270
+};
+
+export interface OrientableSVG extends SVGProps<SVGSVGElement> {
+	orientation?:Orientation|number;
+}
+
+export * from "./ScrollHint";
+export * from "./IsogridBackground";
+export * from "./Icon";
+export * from "./TextEffect";
+export * from "./RainbowText";

eb32a76fdda101d08928cd602967fe0780acf58c
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2024-01-15 17:22:56 +0800

    msg about iframe loading so user doesnt think resume is blank

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -12,9 +12,12 @@ export default function OldIndex() {
         <meta name='topic' content='resume' />
         <meta name='keywords' content='interpause, developer, maker' />
       </Head>
+      <div tw='grid fixed h-full w-full place-items-center'>
+        <h1 tw='text-4xl'>Waiting for Microsoft Word to load...</h1>
+      </div>
       <iframe
         src='https://onedrive.live.com/embed?resid=DAFBAD5E86133D01%21107199&amp;authkey=%21AB_hYJwH9Ufyaao&amp;em=2'
-        tw='absolute inset-0 m-auto h-full max-w-full'
+        tw='fixed inset-0 m-auto h-full max-w-full'
         width='1428px'
         height='864px'
         frameBorder='0'
